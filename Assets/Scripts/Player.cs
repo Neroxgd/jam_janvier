9a1da6edf505ceb0631eb5d660cd949360ddecf2
Neroxgd <darkaux69@gmail.com>
2024-01-23 17:25:12 +0100

    chatouille

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -2,22 +2,27 @@ using System.Collections;
 using UnityEngine;
 using UnityEngine.InputSystem;
 using DG.Tweening;
+using UnityEngine.UI;
 
 public class Player : MonoBehaviour
 {
     private Vector2 direction;
     private Rigidbody rbPlayer;
-    [SerializeField] private float speed = 1f, jumpForce = 1f, gravityForce = 1f, throwForce = 3f;
+    [SerializeField] private float speed = 1f, speedPorteObject = 0.5f, jumpForce = 1f, gravityForce = 1f, throwForce = 3f, chatouillePower = 1f, chatouilleCalme = 0.1f;
     private bool porter;
+    private float currentSpeed;
     private RaycastHit groundHit;
     private Vector3 currentRotation;
     [SerializeField] private LayerMask layerGround;
+    [SerializeField] private Image chatouilleBarre; public Image ChatouilleBarre { get { return chatouilleBarre; } set { chatouilleBarre = value; } }
     private bool notThrow;
+    private bool isChatouilling;
     private Transform currentObjectPorted;
     private bool IsGrounded => Physics.BoxCast(transform.position, new Vector3(0.5f, 0.1f, 0.5f), Vector3.down, out groundHit, transform.rotation, 0.5f, layerGround);
 
     private void Start()
     {
+        currentSpeed = speed;
         rbPlayer = GetComponent<Rigidbody>();
         Physics.gravity = Vector3.down * gravityForce;
     }
@@ -30,9 +35,20 @@ public class Player : MonoBehaviour
             currentRotation = new Vector3(direction.x, 0, direction.y);
     }
 
+    private void Update()
+    {
+        if (isChatouilling)
+        {
+            RaycastHit hitChatouille;
+            if (Physics.SphereCast(transform.position, 0.4f, transform.forward, out hitChatouille, 1f) && hitChatouille.transform.GetComponent<Player>() && hitChatouille.transform != this)
+                hitChatouille.transform.GetComponent<Player>().ChatouilleBarre.fillAmount += chatouillePower / 10f * Time.deltaTime;
+        }
+        chatouilleBarre.fillAmount -= chatouilleCalme / 10f * Time.deltaTime;
+    }
+
     private void FixedUpdate()
     {
-        rbPlayer.velocity = new Vector3(direction.x * speed, rbPlayer.velocity.y, direction.y * speed);
+        rbPlayer.velocity = new Vector3(direction.x * currentSpeed, rbPlayer.velocity.y, direction.y * currentSpeed);
         transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.LookRotation(currentRotation), 0.2f);
     }
 
@@ -45,6 +61,7 @@ public class Player : MonoBehaviour
     public void Porter(InputAction.CallbackContext context)
     {
         if (!context.started || currentObjectPorted != null || porter) return;
+        currentSpeed = speedPorteObject;
         notThrow = true;
         RaycastHit hit;
         if (Physics.SphereCast(transform.position, 0.4f, transform.forward, out hit, 1f))
@@ -55,31 +72,34 @@ public class Player : MonoBehaviour
                 currentObjectPorted = hit.transform;
                 hit.transform.GetComponent<ObjectPortable>().Porter(this);
             }
-            if (hit.transform.GetComponent<Player>() && hit.transform != this)
-            {
-                StartCoroutine(Wait1Frame());
-                currentObjectPorted = hit.transform;
-                currentObjectPorted.transform.parent = transform;
-                currentObjectPorted.GetComponent<Player>().SeFairePorter(this);
-            }
+            // if (hit.transform.GetComponent<Player>() && hit.transform != this)
+            // {
+            //     StartCoroutine(Wait1Frame());
+            //     currentObjectPorted = hit.transform;
+            //     currentObjectPorted.transform.parent = transform;
+            //     currentObjectPorted.GetComponent<Player>().SeFairePorter(this);
+            // }
         }
     }
-
-    // void OnDrawGizmos()
-    // {
-    //     Gizmos.DrawSphere(transform.position + transform.forward / 2f, 0.4f);
-    //     Gizmos.DrawSphere(transform.position + transform.forward * 1.5f, 0.4f);
-    // }
 
     public void Jeter(InputAction.CallbackContext context)
     {
         if (!context.started || currentObjectPorted == null || notThrow || porter) return;
+        currentSpeed = speed;
         Rigidbody rbCurrentObjectPorted = currentObjectPorted.GetComponent<Rigidbody>();
         StartCoroutine(Stun(currentObjectPorted.GetComponent<Player>()));
         rbCurrentObjectPorted.isKinematic = false;
         currentObjectPorted.transform.parent = null;
-        rbCurrentObjectPorted.AddForce(new Vector3(transform.forward.x, transform.forward.y + 2f, transform.forward.z) * throwForce * rbCurrentObjectPorted.mass, ForceMode.Impulse);
+        rbCurrentObjectPorted.AddForce(new Vector3(transform.forward.x, transform.forward.y, transform.forward.z) * throwForce * rbCurrentObjectPorted.mass, ForceMode.Impulse);
         currentObjectPorted = null;
+    }
+
+    public void Chatouiller(InputAction.CallbackContext context)
+    {
+        if (context.started)
+            isChatouilling = true;
+        else if (context.canceled)
+            isChatouilling = false;
     }
 
     private void SeFairePorter(Player player)
@@ -102,5 +122,3 @@ public class Player : MonoBehaviour
         player.enabled = true;
     }
 }
-
-
