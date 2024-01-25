7236deb7032253a804dd7342bd1f2682aecb463f
Neroxgd <darkaux69@gmail.com>
2024-01-25 18:13:15 +0100

    compteur start

--- Assets/Scripts/ObjectPortable.cs
+++ Assets/Scripts/ObjectPortable.cs
@@ -14,8 +14,8 @@ public class ObjectPortable : MonoBehaviour
     public void Porter(Player player)
     {
         _player = player;
-        transform.DOMove(_player.transform.position + Vector3.up * 1.6f, 0.1f)
-        .OnComplete(() => transform.position = _player.transform.position + Vector3.up * 1.6f);
+        transform.DOMove(_player.transform.position + Vector3.up * 1.7f, 0.1f)
+        .OnComplete(() => transform.position = _player.transform.position + Vector3.up * 1.7f);
         rb.isKinematic = true;
         transform.parent = player.transform;
     }
