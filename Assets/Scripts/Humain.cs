49cc72d86cfbed3d47c3f710aa2d637bd5d1ef46
Neroxgd <darkaux69@gmail.com>
2024-01-25 15:49:38 +0100

    chat

--- Assets/Scripts/Humain.cs
+++ Assets/Scripts/Humain.cs
@@ -3,8 +3,13 @@ using DG.Tweening;
 
 public class Humain : MonoBehaviour
 {
+    void Start()
+    {
+        EndGame();
+    }
+
     public void EndGame()
     {
-        transform.DOMove(transform.position + new Vector3(1f, 0, -1f), 0.2f);
+        transform.DOMove(transform.position + new Vector3(2f, 0, -2f), 0.2f);
     }
 }
